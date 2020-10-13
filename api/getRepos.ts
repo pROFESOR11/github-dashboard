34328db94cdf6a749f59542211f7ea3107b025d8
pROFESOR11 <ali_cimen_11@hotmail.com>
2020-10-13 13:50:26 +0300

    getRepos api func

---
+++ api/getRepos.ts
@@ -0,0 +1,32 @@
+import { SearchResponse } from "./types";
+
+const API_BASE_URL = "https://api.github.com";
+
+export async function getRepos(
+  _key: string,
+  queryStr: string,
+  page?: number
+): Promise<SearchResponse> {
+  return fetch(
+    `${API_BASE_URL}/search/repositories?q=${queryStr}&sort=stars&order=desc&page=${
+      page || 1
+    }&per_page=41`
+  )
+    .then((response) => response.json())
+    .then((resj) =>
+      // fetch n+1 items, return n items, then inform client whether there is more to fetch via hasMore key
+      Object.assign(
+        {},
+        {
+          ...resj,
+          items: resj.items.slice(0, 40),
+        },
+        {
+          hasMore: resj.items.length === 41,
+        }
+      )
+    )
+    .catch((err) => {
+      console.log("err", err);
+    });
+}
